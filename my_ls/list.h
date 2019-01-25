eba7c94722eaa77e5deb1d32457df023cbb5535a
Thenarasu <tenarasu090493@gmail.com>
2019-01-25 18:49:16 +0530

    Executing ls command list out the directories and files

---
+++ my_ls/list.h
@@ -0,0 +1,25 @@
+                     //*-------------------------*//
+		    //      SCHOOL OF LINUX        //
+		     //*-------------------------*//
+
+
+	/*----------------------------------------------------*/
+	/*   NAME      	 : THENNARASU            	      */
+	/*   DATE 	 : 25-01-2019	        	      */
+	/*   FILE NAME 	 : list.h	        	      */
+	/*   DESCRIPTION : HEADER FILES DECLARATIONS	      */
+	/*----------------------------------------------------*/
+
+//DECLARATIONS OF HEADER FILES
+
+
+#ifndef  LIST_H
+#define  LIST_H
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <error.h>
+
+#endif			
