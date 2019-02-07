c23772b2d206c35f2ffddd8eaf2dfab9d3580bd0
Thenarasu <tenarasu090493@gmail.com>
2019-02-07 17:25:46 +0530

    pthread_creation

--- thread/pthread.c
+++ thread/pthread.c
@@ -16,7 +16,7 @@
 /* Function defenitions  */
 
 
-static void *threadfun(void *arg)
+static void *threadfun1(void *arg)
 {
 	unsigned long long int *ptr = (unsigned long long int *)arg;
 
@@ -24,30 +24,46 @@ static void *threadfun(void *arg)
 
 	for(i=1;i<=(*ptr);i++)
 	{
-		k+=i;
-		printf("%lld\n",k);
+		k+=(i*i);
 	}
+		printf("%lld\n",k);
 
 }
 
+
 int main(int argc,char*argv[])
+
 {
-	pthread_t t;
-	int s;
-	unsigned long long int input_data1 =4294967298;
-	void *res;
+	pthread_t t1,t2,t3;
+	int s1,s2,s3;
+	unsigned long long int input_data1 =10000000000,input_data2 = input_data1*2,input_data3 =input_data2*2;
+	void *res1,*res2,*res3;
 
 	/* create a new thread   */
 	/* int pthread_create(pthread_t *thread, const pthread_attr_t *attr,void *(*start_routine) (void *), void *arg);*/
 
-	s = pthread_create(&t,NULL,threadfun,&input_data1);
+	s1 = pthread_create(&t1,NULL,threadfun1,&input_data1);
 	
 	perror("pthread :");
 
+	s2 = pthread_create(&t2,NULL,threadfun1,&input_data2);
+
+	perror("pthread:");
+
+	s3 = pthread_create(&t3,NULL,threadfun1,&input_data3);
+
+	perror("pthread:");
+
 	/* join with a terminated thread */
         /* int pthread_join(pthread_t thread, void **retval); */
-	threadfun(&input_data1);
+//	threadfun1(&input_data1);
 
-	s = pthread_join(t,&res);
+	s1 = pthread_join(t1,&res1);
+	perror("pthread_join:");
+	
+	s2 = pthread_join(t2,&res2);
+	perror("pthread_join:");
+	
+	s3 = pthread_join(t3,&res3);
 	perror("pthread_join:");
 }
